ee5f97e7ada1e51305dda7fa99f67d2cbe65a7b1
xavi <javier19pedraza@gmail.com>
2022-12-08 19:50:14 -0500

    finalizing app

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,22 +1,28 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import { render } from 'react-dom';
 
 const CounterApp = ({ value }) => {
 
     const [counter, setCounter] = useState( value )
-
     const handleAdd = () => {
         /* setCounter( counter +1) */
         setCounter( (c) => c + 1 ) /* el valor der retorno va a ser el nuevo counter */
+    }
+    const handleRest = () => {
+        setCounter( counter -1)
+    }
+    const handleReset= () =>{
+        setCounter(value)
     }
 
     return ( 
         <>
             <h1>CounterApp</h1>
             <h2>{ counter }</h2>
-            <button onClick={ handleAdd } > 
-                Click +1
-            </button>
+            <button onClick={ handleRest } >  -1 </button>
+            <button onClick={ handleReset } >  Reset </button>
+            <button onClick={ handleAdd } >  +1 </button>
         </>
     );
 }
